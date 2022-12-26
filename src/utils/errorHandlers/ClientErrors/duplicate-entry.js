224cc9571c07055b7c9eb6fa7832303119eeeb17
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2022-12-26 20:28:55 +0530

    Added some more error handlers

---
+++ src/utils/errorHandlers/ClientErrors/duplicate-entry.js
@@ -0,0 +1,21 @@
+const {StatusCodes} = require('http-status-codes');
+const AppErrors = require('../error-handler');
+
+class DuplicateEntry extends AppErrors{
+    constructor(error){
+        let errorName = error.name;
+        
+        let message = error.errors[0].message;
+
+        let explanation = error.parent.sqlMessage;
+
+        super(
+            errorName,
+            message,
+            explanation,
+            StatusCodes.BAD_REQUEST
+        );
+    }
+}
+
+module.exports = DuplicateEntry;
--- src/utils/errorHandlers/ClientErrors/index.js
+++ src/utils/errorHandlers/ClientErrors/index.js
@@ -2,5 +2,6 @@ module.exports = {
     PasswordMismatchError: require('./password-mismatch'),
     UserNotFoundError: require('./user-not-found-error'),
     ValidationError: require('./validation-error'),
-    TokenVerificationError: require('./token-verification-error')
+    TokenVerificationError: require('./token-verification-error'),
+    DuplicateEntryError: require('./duplicate-entry')
 };
